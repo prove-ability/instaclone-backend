e4197e0b7a7bc4ee23364f9e35f2c51e0501516d
Prove <prove.ability00@gmail.com>
2021-03-28 14:52:58 +0900

    1.0-1 Backend Setup - typescript

---
+++ server.ts
@@ -0,0 +1,24 @@
+import { ApolloServer, gql } from "apollo-server";
+
+// The GraphQL schema
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// A map of functions which return data for the schema.
+const resolvers = {
+  Query: {
+    hello: () => "world",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server
+  .listen()
+  .then(() => console.log("Server is running on http://localhost:4000/"));
