4352b3523646095ace1dda0a8b7f894e06a0d52d
Prove <prove.ability00@gmail.com>
2021-03-30 20:20:41 +0900

    1.1 Backend Setup - use studio

--- server.ts
+++ server.ts
@@ -1,16 +1,50 @@
-import { ApolloServer, gql } from "apollo-server";
+import { PrismaClient } from ".prisma/client";
+import { Movie } from "@prisma/client";
+import { ApolloServer, gql, ServerInfo } from "apollo-server";
+
+const client = new PrismaClient();
 
 // The GraphQL schema
 const typeDefs = gql`
+  type Movie {
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie(id: Int!): Movie
+  }
+  type Mutation {
+    createMovie(title: String!, year: Int!, genre: String): Movie
+    deleteMovie(id: Int!): Movie
+    updateMovie(id: Int!, year: Int!): Movie
   }
 `;
 
 // A map of functions which return data for the schema.
 const resolvers = {
   Query: {
-    hello: () => "world",
+    movies: () => client.movie.findMany(),
+    movie: (_: any, { id }: Movie) =>
+      client.movie.findUnique({ where: { id } }),
+  },
+  Mutation: {
+    createMovie: (_: any, { title, year, genre }: Movie) =>
+      client.movie.create({
+        data: {
+          title,
+          year,
+          genre,
+        },
+      }),
+    deleteMovie: (_: any, { id }: Movie) =>
+      client.movie.delete({ where: { id } }),
+    updateMovie: (_: any, { id, year }: Movie) =>
+      client.movie.update({ where: { id }, data: { year } }),
   },
 };
 
@@ -21,4 +55,4 @@ const server = new ApolloServer({
 
 server
   .listen()
-  .then(() => console.log("Server is running on http://localhost:4000/"));
+  .then(({ url }: ServerInfo) => console.log(`Server is running on ${url}`));
